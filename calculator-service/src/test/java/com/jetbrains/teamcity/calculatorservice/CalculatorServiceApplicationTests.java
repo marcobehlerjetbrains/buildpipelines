7d34ef7c30b2a73664d5095993484ae1f8b604b3
Marco Behler <marco@marcobehler.com>
2021-07-19 13:06:35 +0200

    initial commit

---
+++ calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/CalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.teamcity.calculatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
