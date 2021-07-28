12c909ef6a167e1d2d3e79e476f2c8f1f542e5f5
Marco Behler <marco.behler@jetbrains.com>
2021-07-28 17:29:44 +0200

    Added some more integration tests

--- calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/SomeIntegrationTest.java
+++ calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/SomeIntegrationTest.java
@@ -3,12 +3,26 @@ package com.jetbrains.teamcity.calculatorservice;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SomeIntegrationTest {
 
     @Test
-    public void anIntegrationTest() throws InterruptedException {
+    public void anIntegrationTest() throws InterruptedException, IOException {
+        Path path = Paths.get("./artifacts");
+        Files.createDirectories(path);
+
+        try (InputStream is = SomeIntegrationTest.class.getResourceAsStream("/sample-mp4-file.mp4")) {
+            Files.copy(is, path.resolve("sample.mp4"));
+        }
+        Files.write(path.resolve("a_log.txt"), "This is my log".getBytes());
         Thread.sleep(1000);
         assertEquals(1,1);
     }
