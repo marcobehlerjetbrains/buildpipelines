9ffd30efda1b8cce9fb7c39048aea6b0d8b43b85
Marco Behler <marco.behler@jetbrains.com>
2021-07-28 16:31:09 +0200

    ADded some tests to calculator service

---
+++ calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/AnotherUnitTest.java
@@ -0,0 +1,18 @@
+package com.jetbrains.teamcity.calculatorservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AnotherUnitTest {
+
+    @Test
+    public void something_real_quick() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void something_else_real_quick() {
+        assertEquals(2,2);
+    }
+}
---
+++ calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/SomeIntegrationTest.java
@@ -0,0 +1,15 @@
+package com.jetbrains.teamcity.calculatorservice;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeIntegrationTest {
+
+    @Test
+    public void anIntegrationTest() throws InterruptedException {
+        Thread.sleep(1000);
+        assertEquals(1,1);
+    }
+}
