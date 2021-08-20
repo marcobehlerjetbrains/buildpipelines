0f94ac9d3af08283542d26c16959d6c6dafe7423
Marco Behler <marco.behler@jetbrains.com>
2021-08-20 13:35:18 +0200

    Added some more integration tests

--- calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/SomeIntegrationTest.java
+++ calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/SomeIntegrationTest.java
@@ -61,5 +61,6 @@ public class SomeIntegrationTest {
         System.out.println("test");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("test4");
     }
 }
